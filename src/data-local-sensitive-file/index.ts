7eb879c43725052a60deba2c1de5be954e7593a5
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2025-06-03 12:14:09 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/data-local-sensitive-file/index.ts
+++ src/data-local-sensitive-file/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/hashicorp/local/2.5.3/docs/data-sources/sensitive_file
 // generated from terraform resource schema
 
